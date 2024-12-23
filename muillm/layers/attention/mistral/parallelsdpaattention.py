ee5c3f25402e72141172cbf65ff7a2ab3636e467
Epliz <63452361+Epliz@users.noreply.github.com>
2024-12-23 16:48:17 +0100

    WIP: passing several caches, masks to parallel decoder

--- muillm/layers/attention/mistral/parallelsdpaattention.py
+++ muillm/layers/attention/mistral/parallelsdpaattention.py
@@ -1,5 +1,5 @@
 import math
-from typing import Optional, Tuple, Union
+from typing import List, Optional, Tuple, Union
 import warnings
 from muillm.layers.attention.mistral.baseattention import MuiMistralAttention
 import torch
@@ -43,19 +43,26 @@ class MuiParallelMistralSdpaAttention(MuiParallelMistralAttention):
         return new_module
 
     # Adapted from MistralAttention.forward
-    def forward(
+    def parallel_forward(
         self,
-        query_states: torch.Tensor,
-        key_states: torch.Tensor,
-        value_states: torch.Tensor,
-        attention_mask: Optional[torch.Tensor] = None,
-        position_ids: Optional[torch.LongTensor] = None,
-        past_key_value: Optional[Cache] = None,
+        query_states: Union[torch.Tensor, List[torch.Tensor]],
+        key_states: Union[torch.Tensor, List[torch.Tensor]],
+        value_states: Union[torch.Tensor, List[torch.Tensor]],
+        attention_masks: Optional[List[torch.Tensor]] = None,
+        position_ids: Optional[List[torch.LongTensor]] = None,
+        past_key_values: Optional[List[Cache]] = None,
         output_attentions: bool = False,
         use_cache: bool = False,
         all_ones_mask: Optional[bool] = None,
         residual: Optional[torch.Tensor] = None,
-    ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
+    ) -> Tuple[List[torch.Tensor], Optional[List[torch.Tensor]], Optional[List[Cache]]]:
+        if (attention_masks is not None) and (not isinstance(attention_masks, list)):
+            raise ValueError("attention mask needs to be a list")
+        if (position_ids is not None) and (not isinstance(position_ids, list)):
+            raise ValueError("position_ids needs to be a list")
+        if (past_key_values is not None) and (not isinstance(past_key_values, list)):
+            raise ValueError("must pass list of caches")
+
         if output_attentions:
             # TODO: Improve this warning with e.g. `model.config.attn_implementation = "manual"` once this is implemented.
             logger.warning_once(
@@ -66,72 +73,93 @@ class MuiParallelMistralSdpaAttention(MuiParallelMistralAttention):
                 query_states=query_states,
                 key_states=key_states,
                 value_states=value_states,
-                attention_mask=attention_mask,
+                attention_masks=attention_masks,
                 position_ids=position_ids,
-                past_key_value=past_key_value,
+                past_key_values=past_key_values,
                 output_attentions=output_attentions,
                 use_cache=use_cache,
                 all_ones_mask=all_ones_mask,
             )
 
+        sharded_inputs = isinstance(query_states, list)
+        if not sharded_inputs:
+            query_states = self.__shard_inputs(query_states)
+            key_states = self.__shard_inputs(key_states)
+            value_states = self.__shard_inputs(value_states)
+        else:
+            # already sharded
+            query_states = query_states
+            key_states = key_states
+            value_states = value_states
+
         if all_ones_mask is None:
             # if not specified, assume it might not have just ones
             all_ones_mask = False
 
+        num_head_groups = len(query_states)
 
-        bsz, q_len, _ = query_states.size()
+        bsz, q_len, _ = query_states[0].size()
 
-        query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
-        key_states = key_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
-        value_states = value_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+        query_states = [query_state.view(bsz, q_len, self.num_tp_heads, self.head_dim).transpose(1, 2) for query_state in query_states]
+        key_states = [key_state.view(bsz, q_len, self.num_tp_key_value_heads, self.head_dim).transpose(1, 2) for key_state in key_states]
+        value_states = [value_state.view(bsz, q_len, self.num_tp_key_value_heads, self.head_dim).transpose(1, 2) for value_state in value_states]
 
-        kv_seq_len = key_states.shape[-2]
-        if past_key_value is not None:
-            kv_seq_len += past_key_value.get_usable_length(kv_seq_len, self.layer_idx)
+        kv_seq_len = key_states[0].shape[-2]
+        if past_key_values is not None:
+            kv_seq_len += past_key_values[0].get_usable_length(kv_seq_len, self.layer_idx)
 
-        query_states, key_states, value_states = self.rotary_emb.apply_rotary_pos_emb_write_kv_cache(query_states, key_states, position_ids, kv_seq_len, value_states, past_key_value)
+        for d in range(len(query_states)):
+            # do the rotary embeddings on each head group
+            pos_ids = position_ids[d] if position_ids is not None else None
+            past_key_value = past_key_values[d] if past_key_values is not None else None
+            query_states[d], key_states[d], value_states[d] = self.rotary_emb.apply_rotary_pos_emb_write_kv_cache(query_states[d], key_states[d], pos_ids, kv_seq_len, value_states[d], past_key_value)
 
         # at this point, we have the following shapes:
         #  q: [B, num_q_heads, T, embed_dim]
         #  k: [B, num_k_heads, NEW_T, embed_dim]
         #  v: [B, num_v_heads, NEW_T, embed_dim]
-        if (bsz == 1) and (q_len == 1) and all_ones_mask and (query_states.dtype == torch.float16):
+        if (bsz == 1) and (q_len == 1) and all_ones_mask and (query_states[0].dtype == torch.float16):
             #
-            attn_output = mui_causally_decode(query_states, key_states, value_states)
+            attn_outputs = [mui_causally_decode(query_states[d], key_states[d], value_states[d]) for d in range(num_head_groups)]
         else:
-            key_states = repeat_kv(key_states, self.num_key_value_groups)
-            value_states = repeat_kv(value_states, self.num_key_value_groups)
+            attn_outputs = []
+            for d in range(num_head_groups):
+                key_state = repeat_kv(key_states[d], self.num_key_value_groups)
+                value_state = repeat_kv(value_states[d], self.num_key_value_groups)
+                query_state = query_states[d]
 
-            if attention_mask is not None:
-                if attention_mask.size() != (bsz, 1, q_len, kv_seq_len):
-                    raise ValueError(
-                        f"Attention mask should be of size {(bsz, 1, q_len, kv_seq_len)}, but is {attention_mask.size()}"
-                    )
+                attention_mask = None
+                if attention_masks is not None:
+                    attention_mask = attention_masks[d]
+                    if attention_mask.size() != (bsz, 1, q_len, kv_seq_len):
+                        raise ValueError(
+                            f"Attention mask should be of size {(bsz, 1, q_len, kv_seq_len)}, but is {attention_mask.size()}"
+                        )
 
-            # SDPA with memory-efficient backend is currently (torch==2.1.2) bugged with non-contiguous inputs with custom attn_mask,
-            # Reference: https://github.com/pytorch/pytorch/issues/112577.
-            if query_states.device.type == "cuda" and attention_mask is not None:
-                query_states = query_states.contiguous()
-                key_states = key_states.contiguous()
-                value_states = value_states.contiguous()
+                # SDPA with memory-efficient backend is currently (torch==2.1.2) bugged with non-contiguous inputs with custom attn_mask,
+                # Reference: https://github.com/pytorch/pytorch/issues/112577.
+                if query_state.device.type == "cuda" and attention_masks is not None:
+                    query_state = query_state.contiguous()
+                    key_state = key_state.contiguous()
+                    value_state = value_state.contiguous()
 
-            attn_output = torch.nn.functional.scaled_dot_product_attention(
-                query_states,
-                key_states,
-                value_states,
-                attn_mask=attention_mask,
-                dropout_p=self.attention_dropout if self.training else 0.0,
-                # The q_len > 1 is necessary to match with AttentionMaskConverter.to_causal_4d that does not create a causal mask in case q_len == 1.
-                is_causal=self.is_causal and attention_mask is None and q_len > 1,
-            )
+                attn_output = torch.nn.functional.scaled_dot_product_attention(
+                    query_state,
+                    key_state,
+                    value_state,
+                    attn_mask=attention_mask,
+                    dropout_p=self.attention_dropout if self.training else 0.0,
+                    # The q_len > 1 is necessary to match with AttentionMaskConverter.to_causal_4d that does not create a causal mask in case q_len == 1.
+                    is_causal=self.is_causal and attention_mask is None and q_len > 1,
+                )
+
+                attn_outputs.append(attn_output)
 
         # from shape [B, num_q_heads, T, embed_dim], go to [B, T, num_q_heads, embed_dim]
-        attn_output = attn_output.transpose(1, 2).contiguous()
-        # from shape [B, T, num_q_heads, embed_dim] go to [B, T, hidden_size]
-        attn_output = attn_output.view(bsz, q_len, self.hidden_size)
+        # then from shape [B, T, num_q_heads, embed_dim] go to [B, T, hidden_size]
+        attn_outputs = [attn_output.transpose(1, 2).contiguous().reshape(bsz, q_len, self.tp_hidden_size) for attn_output in attn_outputs]
 
-        # when non-batched, could push the o_proj into v?
-        # TODO: could be made 2x faster?
-        attn_output = self.o_proj(attn_output, residual=residual)
+        attn_outputs = self.o_proj.parallel_forward(attn_outputs, residual=residual)
 
-        return attn_output, None, past_key_value
+        return attn_outputs, None, past_key_values
+        
