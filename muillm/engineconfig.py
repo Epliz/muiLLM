3ed4ef0fbd1bfed17e1b6b1dd2b36ce6df265e1f
Epliz <63452361+Epliz@users.noreply.github.com>
2024-12-23 12:33:56 +0100

    Make quantization method optional parameter in MuiEngineConfig

--- muillm/engineconfig.py
+++ muillm/engineconfig.py
@@ -1,10 +1,11 @@
+from typing import Optional
 from muillm.quantization.quantizationmethod import QuantizationMethod
 from muillm.synchronization.synchronizer import Synchronizer
 
 class MuiEngineConfig:
     def __init__(
             self,
-            quantization_method: QuantizationMethod
+            quantization_method: Optional[QuantizationMethod] = None
             ):
         self.synchronizer = Synchronizer()
         self.quantization_method = quantization_method
