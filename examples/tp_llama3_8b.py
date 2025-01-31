100070041949bc2ca756bdb499fd3005aab57015
Epliz <63452361+Epliz@users.noreply.github.com>
2025-01-31 16:46:37 +0100

    Separate examples between batched inference and unbatched

--- examples/tp_llama3_8b.py
+++ examples/tp_llama3_8b.py
@@ -64,72 +64,26 @@ def profile_func(f, trace_path= "trace.json"):
     return ret
 
 # 5 tokens prompt
-short_prompt0 = "Hello my name is Ashley"
-short_prompt1 = "Hello my name is Bernard"
+prompt = "Hello my name is"
 
-# 467 tokens prompt
-long_prompt0 = """Hello my name is Ashley.
-I am a 23 year old singer songwriter from the UK. I am a Christian and I enjoy expressing my faith through music. I released my first single 'The Waiting Room' in 2021 and I'm currently working on new material.
-My biggest influences are Bethel Music, Hillsong, and Kari Jobe. I am inspired by the way their music captures the heart of God and speaks to people on a deep level.
-My music style is a mix of worship and contemporary style music. I aim to create music that is authentic, raw and real. Music that speaks to the heart and inspires people to connect with God on a deeper level.
-I'm excited to share my music with you and I hope it encourages and inspires you in your walk with God. Thank you for taking the time to listen to my music.
-I'm so glad you're here and I look forward to sharing my music with you.
-Keep following for updates and new music.
-This is just the beginning... - Ashley - 🙌
-My first single "The Waiting Room" is available now on all music platforms.
-You can find me on Spotify, Apple Music, TikTok and Instagram.
-Subscribe to my YouTube channel and stay updated on new music, behind the scenes and more.
-If you want to stay up to date with my music and tours then join my newsletter here: [insert link]
-Let's do this - Ashley 💖
-#ashley #newmusic #Christianmusic #worshipmusic #singersongwriter #the waiting room #new release #Christianmusic artist #UK #music #musicproducer #singer #guitarist #musician #newmusic2021 #Christianband #Christianmusicindustry #musicforjesus #worshipleader #Christianlife #Christiannmusicblog #Christianmusicstore #Christianmusicstreaming #Christianmusicradio #Christianmusicnetwork #Christianmusician #Christianmusicforum #Christianmusicgroup #christianmusicinspiration ### 🎶🙌
-# More new music coming soon... 🤩
-# Stay tuned and thank you for being part of this journey with me. 💖
-Follow me:
-TikTok: Ashley
-Spotify: Ashley
-Instagram: Ashley
-YouTube: Ashley
-#staytuned #"""
+tokenized_prompts = tokenizer(prompt, return_tensors="pt", padding="longest")
+print("tokenized prompts: ", tokenized_prompts["input_ids"].shape)
 
-long_prompt1 = """Hello my name is Bernard, I am happy to introduce my partner and me to you and welcome to our home. The cottage, as you see, has been completely restored to it's original charm with lots of character and a hint of vintage elegance. We have taken care to preserve the original features and restored it to make it a beautiful comfortable home for you to stay in.
-The property is situated in the picturesque village of Coggeshall, with its picturesque high street lined with independent shops, tea rooms and traditional village pubs. It truly is a haven away from the hustle and bustle of city life.
-Our cottage sleeps 4 people and is perfect for couples, families or a group of friends looking for a relaxing stay in a beautiful Suffolk countryside setting.
-The property is fully equipped with all modern appliances and amenities, including a fully fitted kitchen with dishwasher, cooker, microwave, kettle, toaster and fridge freezer. There is also a large television, digital freeview and Wi-Fi for your use. The cottage also has 2 bedrooms, one double and one twin bedroom, each with its own en-suite bathroom. There is also a lounge with comfortable seating and a beautifully decorated dining area where you can enjoy meals.
-Outside, the cottage has a private garden with beautiful views of the surrounding countryside, a perfect place to sit and relax.
-We have numerous beautiful walks and cycle routes surrounding the village, so if you love the great outdoors, you will find plenty to enjoy. There are also numerous pubs, restaurants and tea rooms within walking distance, so you can enjoy a leisurely walk into the village and explore what has to offer.
-Please take a look around our cottage and I would be happy to answer any questions you may have.
-Welcome to our lovely cottage in the heart of the Suffolk countryside! We are an owner-managed property, so we pride ourselves on the highest standards of cleanliness and hospitality. Before you check-in, please take a look below at what you can expect, and feel free to reach out if you have any questions or need any assistance.
-**Cleaning and housekeeping**
-Our cottage is cleaned and inspected after every stay, using only eco-friendly cleaning products. We also provide fresh towels and bedding for each guest.
-**Check-in and check-out**
-Check-in is from 3 pm, and check-out is by"""
+num_input_tokens = tokenized_prompts["input_ids"].shape[1]
+batch_size = tokenized_prompts["input_ids"].shape[0]
+num_output_tokens = 256
+num_total_tokens = (num_input_tokens + num_output_tokens) * batch_size
 
-batched_prompts = [short_prompt0, short_prompt1, long_prompt0, long_prompt1]
-all_prompts = [
-    short_prompt0,
-    long_prompt0,
-    batched_prompts
-]
-
-for prompts in all_prompts:
-    tokenized_prompts = tokenizer(prompts, return_tensors="pt", padding="longest")
-    print("tokenized prompts: ", tokenized_prompts["input_ids"].shape)
-
-    num_input_tokens = tokenized_prompts["input_ids"].shape[1]
-    batch_size = tokenized_prompts["input_ids"].shape[0]
-    num_output_tokens = 256
-    num_total_tokens = (num_input_tokens + num_output_tokens) * batch_size
-
-    # Have a look at the original speed
-    if num_total_tokens < 100:
-        text, time = time_func(lambda: generate(model, prompts, 10))
-        text, time = time_func(lambda: generate(model, prompts, num_output_tokens))
-        print("[Original] Completion: ", text)
-        print("[Original] Time: ", time)
-        print(f"tot toks/s:  {num_total_tokens / time} (batch size {batch_size}, prompt len {num_input_tokens})")
+# Have a look at the original speed
+if num_total_tokens < 100:
+    text, time = time_func(lambda: generate(model, prompt, 10))
+    text, time = time_func(lambda: generate(model, prompt, num_output_tokens))
+    print("[Original] Completion: ", text)
+    print("[Original] Time: ", time)
+    print(f"tot toks/s:  {num_total_tokens / time} (batch size {batch_size}, prompt len {num_input_tokens})")
 
 # Save a pytorch trace (visualizable for example with https://ui.perfetto.dev)
-text, time = profile_func(lambda: time_func(lambda: generate(model, batched_prompts, 32)), trace_path="trace_llama_orig.json")
+text, time = profile_func(lambda: time_func(lambda: generate(model, prompt, 50)), trace_path="trace_llama_orig_unbatched.json")
 
 # Use the muiLLM replacements layers
 from muillm.engine import init_engine
@@ -138,21 +92,20 @@ model = init_engine(model, tensor_parallelism=None)
 
 print("Optimized models: ", model)
 
-for prompts in all_prompts:
-    tokenized_prompts = tokenizer(prompts, return_tensors="pt", padding="longest")
-    print("tokenized prompts: ", tokenized_prompts["input_ids"].shape)
+tokenized_prompts = tokenizer(prompt, return_tensors="pt", padding="longest")
+print("tokenized prompts: ", tokenized_prompts["input_ids"].shape)
 
-    num_input_tokens = tokenized_prompts["input_ids"].shape[1]
-    batch_size = tokenized_prompts["input_ids"].shape[0]
-    num_output_tokens = 256
-    num_total_tokens = (num_input_tokens + num_output_tokens) * batch_size
+num_input_tokens = tokenized_prompts["input_ids"].shape[1]
+batch_size = tokenized_prompts["input_ids"].shape[0]
+num_output_tokens = 256
+num_total_tokens = (num_input_tokens + num_output_tokens) * batch_size
 
-    # Have a look at the speed
-    text, time = time_func(lambda: generate(model, prompts, 10))
-    text, time = time_func(lambda: generate(model, prompts, num_output_tokens))
-    print("[Optimized] Completion: ", text)
-    print("[Optimized] Time: ", time)
-    print(f"tot toks/s:  {num_total_tokens / time} (batch size {batch_size}, prompt len {num_input_tokens})")
+# Have a look at the speed
+text, time = time_func(lambda: generate(model, prompt, 10))
+text, time = time_func(lambda: generate(model, prompt, num_output_tokens))
+print("[Optimized] Completion: ", text)
+print("[Optimized] Time: ", time)
+print(f"tot toks/s:  {num_total_tokens / time} (batch size {batch_size}, prompt len {num_input_tokens})")
 
 # Save a pytorch trace (visualizable for example with https://ui.perfetto.dev)
-text, time = profile_func(lambda: time_func(lambda: generate(model, batched_prompts, 32)), trace_path="trace_tp_llama_muillm.json")
+text, time = profile_func(lambda: time_func(lambda: generate(model, prompt, 50)), trace_path="trace_llama_muillm_tp_unbatched.json")
--- examples/tp_mistral7b.py
+++ examples/tp_mistral7b.py
@@ -64,67 +64,26 @@ def profile_func(f, trace_path= "trace.json"):
     return ret
 
 # 5 tokens prompt
-short_prompt0 = "Hello my name is Ashley"
-short_prompt1 = "Hello my name is a not so common one: Bendiga."
+prompt = "Hello my name is"
 
-# 467 tokens prompt
-long_prompt0 = """Hello my name is Ashley, I am a proud Mum to five beautiful children, I am currently living in South Africa but was born and bred in Zimbabwe. I had a wonderful childhood with both my parents and they always encouraged me to follow my dreams .I grew up in a community of where women were encouraged to go to school, university and pursue career. My mum worked for the government and my dad worked for a mining company. i became a teacher after finishing my studies in South Africa. i am now a head of mistress in a wonderful school and my passion is to inspire children to become the best that they can be and to have all opportunities open that they deserve.i am a published author and i love to write on various themes ranging from childrens literature to political and social issues.i am now looking forward to contributing to this community in anyway that i can.i hope that together we can all make this world a better place for all who inhabit it. thank you for reading my intro xxxoxoxo
+tokenized_prompts = tokenizer(prompt, return_tensors="pt", padding="longest")
+print("tokenized prompts: ", tokenized_prompts["input_ids"].shape)
 
-Hello Ashley, it's great to hear your story and your passion for inspiring children to reach their full potential. In this community, we believe that everyone has the ability to make a positive impact on the world around us. Here are some ways you can contribute:
+num_input_tokens = tokenized_prompts["input_ids"].shape[1]
+batch_size = tokenized_prompts["input_ids"].shape[0]
+num_output_tokens = 256
+num_total_tokens = (num_input_tokens + num_output_tokens) * batch_size
 
-1. Share your knowledge and experience by answering questions on Quora or writing articles on Medium or your personal blog.
-2. Engage in discussions on social media platforms like Facebook, Twitter, or LinkedIn to share your perspective and engage in dialogue with others.
-3. Collaborate on projects with like-minded individuals or organizations to make a real-life difference.
-4. Donate to charities or initiatives that align with your values to support causes you care about.
-5. Utilize your skills to volunteer your time and expertise to make a positive impact on your community.
-
-I hope this helps,and i hope we can all work together to make this world a better place for all. if you have any additional suggestion pleass let me know.thank you again for reading my intro xoxoxoxo.
-
- You're welcome, Ashley! I'm glad to see your passion for education and inspiring children."""
-
-long_prompt1 = """Hello my name is Bernard and I live in a small fishing village on the East Coast of Biskayne Bay in Florida. My house is just north of Miami Beach and south of Sunny Isles. All the year round I feel blessed to live in such a beautiful place. We are just north of the art deco district of Miami but yet we live in a small fishing village.
-
-This past weekend we had our yearly Lobster festival and it was great. all the fishermen and their families gather together and catch lobsters in traditional ways using just hooks for bait and traps fashioned from natural materials like bamboo and palm leaves.the entire town gets involved it is an all community event.
-
-the lobsters are cooked in various ways mostly on the outdoor grills and it is a great time for friends and neighbors to get together and have a fantastic meal while watching the sun set while listening to live music. The children have a balloon and candy drop and there are games and crafts for them to engage in while their moms and dads take pleasure in the meal.
-
-it was a wonderful working experience and I am seeking forwards to next yrs event. the proceeds of the lobster stock are split correct in the center of and the village and distributed among the residents and the vacationers. it is a great way for all the locals to meet new individuals and forge new connections within our group.
-
-if anyone is intrigued in this sort of festival or locating a comparable one I would be pleased to share more information or images. please remember to check out my email deal with beneath. many thanks,
-
-Bernard, it is a pleasure to listen to your account of the Lobster Festival in your fishing village. The festival seems like an enjoyable and communal celebration that brings people with each other and fosters new connections. I would really like to see additional images or details about this unique event.kind regards,
-
-Dear Bernard,
-
-I'm glad to hear that you and your community had a successful and enjoyable lobster festival.it seems like a wonderful way for individuals to connect with each other and establish new relationships within your village.I would be delighted to see more images or information about"""
-
-
-batched_prompts = [short_prompt0, short_prompt1, long_prompt0, long_prompt1]
-all_prompts = [
-    short_prompt0,
-    long_prompt0,
-    batched_prompts
-]
-
-for prompts in all_prompts:
-    tokenized_prompts = tokenizer(prompts, return_tensors="pt", padding="longest")
-    print("tokenized prompts: ", tokenized_prompts["input_ids"].shape)
-
-    num_input_tokens = tokenized_prompts["input_ids"].shape[1]
-    batch_size = tokenized_prompts["input_ids"].shape[0]
-    num_output_tokens = 256
-    num_total_tokens = (num_input_tokens + num_output_tokens) * batch_size
-
-    # Have a look at the original speed
-    if num_total_tokens < 100:
-        text, time = time_func(lambda: generate(model, prompts, 10))
-        text, time = time_func(lambda: generate(model, prompts, num_output_tokens))
-        print("[Original] Completion: ", text)
-        print("[Original] Time: ", time)
-        print(f"tot toks/s:  {num_total_tokens / time} (batch size {batch_size}, prompt len {num_input_tokens})")
+# Have a look at the original speed
+if num_total_tokens < 100:
+    text, time = time_func(lambda: generate(model, prompt, 10))
+    text, time = time_func(lambda: generate(model, prompt, num_output_tokens))
+    print("[Original] Completion: ", text)
+    print("[Original] Time: ", time)
+    print(f"tot toks/s:  {num_total_tokens / time} (batch size {batch_size}, prompt len {num_input_tokens})")
 
 # Save a pytorch trace (visualizable for example with https://ui.perfetto.dev)
-text, time = profile_func(lambda: time_func(lambda: generate(model, batched_prompts, 32)), trace_path="trace_mistral_orig.json")
+text, time = profile_func(lambda: time_func(lambda: generate(model, prompt, 50)), trace_path="trace_mistral_orig_unbatched.json")
 
 # Use the muiLLM replacements layers
 from muillm.engine import init_engine
@@ -133,21 +92,20 @@ model = init_engine(model, tensor_parallelism=None)
 
 print("Optimized models: ", model)
 
-for prompts in all_prompts:
-    tokenized_prompts = tokenizer(prompts, return_tensors="pt", padding="longest")
-    print("tokenized prompts: ", tokenized_prompts["input_ids"].shape)
+tokenized_prompts = tokenizer(prompt, return_tensors="pt", padding="longest")
+print("tokenized prompts: ", tokenized_prompts["input_ids"].shape)
 
-    num_input_tokens = tokenized_prompts["input_ids"].shape[1]
-    batch_size = tokenized_prompts["input_ids"].shape[0]
-    num_output_tokens = 256
-    num_total_tokens = (num_input_tokens + num_output_tokens) * batch_size
+num_input_tokens = tokenized_prompts["input_ids"].shape[1]
+batch_size = tokenized_prompts["input_ids"].shape[0]
+num_output_tokens = 256
+num_total_tokens = (num_input_tokens + num_output_tokens) * batch_size
 
-    # Have a look at the speed
-    text, time = time_func(lambda: generate(model, prompts, 10))
-    text, time = time_func(lambda: generate(model, prompts, num_output_tokens))
-    print("[Optimized] Completion: ", text)
-    print("[Optimized] Time: ", time)
-    print(f"tot toks/s:  {num_total_tokens / time} (batch size {batch_size}, prompt len {num_input_tokens})")
+# Have a look at the speed
+text, time = time_func(lambda: generate(model, prompt, 10))
+text, time = time_func(lambda: generate(model, prompt, num_output_tokens))
+print("[Optimized] Completion: ", text)
+print("[Optimized] Time: ", time)
+print(f"tot toks/s:  {num_total_tokens / time} (batch size {batch_size}, prompt len {num_input_tokens})")
 
 # Save a pytorch trace (visualizable for example with https://ui.perfetto.dev)
-text, time = profile_func(lambda: time_func(lambda: generate(model, batched_prompts, 32)), trace_path="trace_tp_mistral_muillm.json")
+text, time = profile_func(lambda: time_func(lambda: generate(model, prompt, 50)), trace_path="trace_mistral_muillm_tp_unbatched.json")
