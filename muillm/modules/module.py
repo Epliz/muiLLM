4ab0f8fa2fec6ef348bf126eed66b6cb19d368cb
Epliz <63452361+Epliz@users.noreply.github.com>
2025-02-19 08:40:43 +0100

    add finalize_module API in MuiModule to make it easier to have correct cpp modules

--- muillm/modules/module.py
+++ muillm/modules/module.py
@@ -7,3 +7,8 @@ class MuiModule(nn.Module):
         nn.Module.__init__(self, **kargs)
 
         self.engine_config = engine_config
+
+    def finalize_init(self) -> None:
+        # Method called at the end of replacements of all layers
+        # can for example build the C++ module counterparts
+        pass
