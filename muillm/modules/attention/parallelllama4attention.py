1598fd1aeff7b18b46e6750a4f277fb0bbed6e3d
Epliz <63452361+Epliz@users.noreply.github.com>
2025-05-08 09:00:33 +0200

    Replace Llama 4 decoder layer

--- muillm/modules/attention/parallelllama4attention.py
+++ muillm/modules/attention/parallelllama4attention.py
@@ -115,7 +115,6 @@ class MuiParallelLlama4TextAttention(MuiModule):
         self,
         engine_config: MuiEngineConfig,
         prev_module: Llama4TextAttention,
-        qkv_proj: MuiParallelMultiLinear,
     ):
         super().__init__(engine_config=engine_config)
         self.config = prev_module.config
@@ -131,7 +130,6 @@ class MuiParallelLlama4TextAttention(MuiModule):
         self.attention_dropout = prev_module.attention_dropout
         self.is_causal = True
         self.use_rope = prev_module.use_rope
-        self.qkv_proj = qkv_proj
         self.o_proj = prev_module.o_proj
         if self.config.use_qk_norm and self.use_rope:
             self.qk_norm = prev_module.qk_norm
@@ -141,39 +139,24 @@ class MuiParallelLlama4TextAttention(MuiModule):
         prev_module: Llama4TextAttention, engine_config: MuiEngineConfig, device=None
     ) -> "MuiParallelLlama4TextAttention":
 
-        q_proj = prev_module.q_proj
-        k_proj = prev_module.k_proj
-        v_proj = prev_module.v_proj
-
-        # we shard by columns because the full attention block is not shardable by head
-        # if there is the QK norm
-        qkv_proj = MuiParallelMultiLinear.replace(
-            prev_modules=[q_proj, k_proj, v_proj],
-            engine_config=engine_config,
-            sharding_dim=1,
-            device=device,
-        )
-
         return MuiParallelLlama4TextAttention(
             engine_config=engine_config,
             prev_module=prev_module,
-            qkv_proj=qkv_proj,
         )
 
     def forward(
         self,
-        hidden_states: torch.Tensor,
+        query_states: torch.Tensor,
+        key_states: torch.Tensor,
+        value_states: torch.Tensor,
         position_embeddings: Tuple[torch.Tensor, torch.Tensor],
         attention_mask: Optional[torch.Tensor],
         past_key_value: Optional[Cache] = None,
         cache_position: Optional[torch.LongTensor] = None,
         **kwargs: Unpack[FlashAttentionKwargs],
     ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
-        input_shape = hidden_states.shape[:-1]
 
-        bsz, q_len, _ = hidden_states.size()
-
-        query_states, key_states, value_states = self.qkv_proj(hidden_states)
+        bsz, q_len, _ = query_states.size()
 
         if (
             (q_len == 1)
---
+++ muillm/modules/decoder/parallelllama4decoder.py
@@ -0,0 +1,182 @@
+from typing import Optional, Tuple, Union
+from muillm.engineconfig import MuiEngineConfig
+from muillm.memorymanagement.gc import trigger_gc
+from muillm.modules.attention.parallelllama4attention import (
+    MuiParallelLlama4TextAttention,
+)
+from muillm.modules.module import MuiModule
+
+import torch
+
+from transformers.models.llama4.modeling_llama4 import (
+    Llama4TextAttention,
+    Llama4TextDecoderLayer,
+)
+
+from muillm.modules.parallelmultilinear import MuiParallelMultiLinear
+
+
+class MuiParallelLlama4TextDecoderLayer(MuiModule):
+    def __init__(
+        self,
+        engine_config: MuiEngineConfig,
+        prev_module: Llama4TextDecoderLayer,
+        qkv_proj: MuiParallelMultiLinear,
+        self_attn: MuiParallelLlama4TextAttention,
+    ):
+        super().__init__(engine_config=engine_config)
+
+        self.hidden_size = prev_module.hidden_size
+        self.self_attn = self_attn
+        self.use_chunked_attention = prev_module.use_chunked_attention  # <=> use rope
+        self.is_moe_layer = (
+            prev_module.is_moe_layer
+        )  # the 128E model interleaves dense / sparse
+
+        self.feed_forward = prev_module.feed_forward
+
+        self.qkv_proj = qkv_proj
+        # TODO: fuse this
+        self.input_layernorm = prev_module.input_layernorm
+        self.post_attention_layernorm = prev_module.post_attention_layernorm
+
+        self.layer_idx = prev_module.layer_idx
+
+    @staticmethod
+    def replace(
+        prev_module: Union[Llama4TextDecoderLayer, "MuiParallelLlama4TextDecoderLayer"],
+        engine_config: MuiEngineConfig,
+        device=None,
+    ) -> "MuiParallelLlama4TextDecoderLayer":
+        if device is None:
+            raise ValueError("device was None")
+
+        if isinstance(prev_module, MuiParallelLlama4TextDecoderLayer):
+            # nothing would be changing if we created a new module, so might as well return the previous one
+            return prev_module
+
+        prev_attn = prev_module.self_attn
+
+        # severe the reference to be able to delete the previous module
+        prev_module.self_attn = None
+
+        qkv_proj = None
+        new_attention = None
+        if isinstance(prev_attn, Llama4TextAttention):
+            prev_q, prev_k, prev_v = (
+                prev_attn.q_proj,
+                prev_attn.k_proj,
+                prev_attn.v_proj,
+            )
+
+            # severe the reference to be able to delete the previous module
+            prev_attn.q_proj = None
+            prev_attn.k_proj = None
+            prev_attn.v_proj = None
+
+            qkv_proj = MuiParallelMultiLinear.replace(
+                prev_modules=[prev_q, prev_k, prev_v],
+                engine_config=engine_config,
+                device=device,
+                sharding_dim=1,  # should switch to row-wise sharding once we split the attention heads
+            )
+
+            del prev_q
+            del prev_k
+            del prev_v
+
+            # trigger GC to save memory
+            trigger_gc()
+
+            new_attention = MuiParallelLlama4TextAttention.replace(
+                prev_attn, engine_config=engine_config, device=device
+            )
+
+            del prev_attn
+
+            # trigger GC to save memory
+            trigger_gc()
+        else:
+            raise ValueError(f"Not supported {type(prev_module.self_attn)}")
+
+        new_module = MuiParallelLlama4TextDecoderLayer(
+            engine_config=engine_config,
+            prev_module=prev_module,
+            qkv_proj=qkv_proj,
+            self_attn=new_attention,
+        )
+
+        # delete the previous module to save memory
+        del prev_module
+
+        # trigger GC to save memory
+        trigger_gc()
+
+        return new_module
+
+    # TODO: parallel forward
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        chunk_causal_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_value: Optional[Tuple[torch.Tensor]] = None,
+        output_attentions: Optional[bool] = False,
+        output_router_logits: Optional[bool] = False,
+        use_cache: Optional[bool] = False,
+        cache_position: Optional[torch.LongTensor] = None,
+        position_embeddings: Optional[
+            Tuple[torch.Tensor, torch.Tensor]
+        ] = None,  # necessary, but kept here for BC
+        **kwargs,
+    ) -> Tuple[
+        torch.FloatTensor, Optional[Tuple[torch.FloatTensor, torch.FloatTensor]]
+    ]:
+        residual = hidden_states
+
+        hidden_states = self.input_layernorm(hidden_states)
+
+        # Transform q, k, v
+        # input layer norm is fused
+        query_states, key_states, value_states = self.qkv_proj(hidden_states)
+
+        # use local attention mask for ROPE layers
+        if self.use_chunked_attention and chunk_causal_mask is not None:
+            attention_mask = chunk_causal_mask
+
+        # Self Attention
+        attention_states, self_attn_weights = self.self_attn(
+            query_states=query_states,
+            key_states=key_states,
+            value_states=value_states,
+            position_embeddings=position_embeddings,
+            attention_mask=attention_mask,
+            past_key_value=past_key_value,
+            output_attentions=output_attentions,
+            use_cache=use_cache,
+            cache_position=cache_position,
+            **kwargs,
+        )
+        hidden_states = residual + attention_states
+
+        # Fully Connected
+        residual = hidden_states
+
+        hidden_states = self.post_attention_layernorm(hidden_states)
+        hidden_states = self.feed_forward(hidden_states)
+        if self.is_moe_layer:
+            hidden_states, router_logits = hidden_states
+        else:
+            router_logits = None
+        hidden_states = residual + hidden_states.view(residual.shape)
+        outputs = (hidden_states,)
+
+        if output_attentions:
+            outputs += (self_attn_weights,)
+
+        if output_router_logits:
+            outputs += (router_logits,)
+
+        return outputs
